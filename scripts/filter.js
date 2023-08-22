795d296b8a10c33da2e46568a8eacb30df2f5269
ls1955 <xxlsxx1955@gmail.com>
2023-08-22 08:29:16 +0800

    Include content script draft

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,1 +1,22 @@
-console.log("Goodbye, world");
+// Might want to move this somewhere else...
+const blacklist = [
+    "nijisanji",
+    "hololive",
+    /a/
+];
+
+blacklist.forEach(tag => console.log(typeof tag));
+
+const imageLists = document.querySelector(".image-list");
+const images = imageLists.querySelectorAll("span");
+
+const filteredImages = [];
+
+images.forEach(image => () => {
+    if isBlackList? image {
+        filteredImages.push(image);
+    }
+});
+
+filteredImages.forEach(image => hide the image)
+
