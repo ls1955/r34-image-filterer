657979defb51cfcd50d411acefb6a578cddeb4fc
ls1955 <xxlsxx1955@gmail.com>
2023-08-22 20:10:59 +0800

    Implement filter.js
    This commit:
    * Select posts from webpage
    * Collect and hide unwanted posts

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,22 +1,23 @@
 // Might want to move this somewhere else...
-const blacklist = [
-    "nijisanji",
-    "hololive",
-    /a/
-];
+const blacklist = new Set(["nijisanji", "hololive"]);
 
-blacklist.forEach(tag => console.log(typeof tag));
+const imageList = document.querySelector(".image-list");
+const posts = imageList.querySelectorAll("span");
 
-const imageLists = document.querySelector(".image-list");
-const images = imageLists.querySelectorAll("span");
+const unwantedPosts = [];
 
-const filteredImages = [];
-
-images.forEach(image => () => {
-    if isBlackList? image {
-        filteredImages.push(image);
-    }
+posts.forEach(post => {
+    if (isInBlackList(post) == true) unwantedPosts.push(post);
 });
 
-filteredImages.forEach(image => hide the image)
+unwantedPosts.forEach(post => post.style.display = "None");
 
+function isInBlackList(post) {
+    // It is observed that alt and title attribute contain tags
+    let tags = post.querySelector("img").alt.trim().split(" ");
+
+    for (let i = 0; i < tags.length; i++) {
+        if (blacklist.has(tags[i])) return true;
+    }
+    return false;
+}
