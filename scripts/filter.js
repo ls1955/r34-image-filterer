b3fc260b3a7ad8afa2ac71ed0c765700844ca17e
Cheong <xxlsxx1955@gmail.com>
2024-02-28 18:00:03 +0800

    Define tags in `blacklist-tags.json`
    To avoid clutter the `src/filter.js` in case there are too many tags.
    
    This commit also:
    * Use `prettier` formatter to format the files.

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,21 +1,29 @@
-// Might want to move this somewhere else...
-const blacklist = new Set(["nijisanji", "hololive"]);
+async function main() {
+  const blackListTags = await getBlacklistTags();
+  const blacklist = new Set(blackListTags);
 
-const imageList = document.querySelector(".image-list");
+  const imageList = document.querySelector(".image-list");
+  const posts = imageList?.querySelectorAll("span");
+  const unwantedPosts = [];
 
-const posts = imageList?.querySelectorAll("span");
+  posts?.forEach((post) => {
+    if (isInBlackList(post, blacklist)) unwantedPosts.push(post);
+  });
 
-const unwantedPosts = [];
-
-posts?.forEach(post => {
-    if (isInBlackList(post)) unwantedPosts.push(post);
-});
-
-unwantedPosts.forEach(post => post.style.display = "None");
-
-function isInBlackList(post) {
-    // It is observed that alt and title attribute contain tags
-    let tags = post.querySelector("img").alt.trim().split(" ");
-
-    return tags.some(tag => blacklist.has(tag));
+  unwantedPosts.forEach((post) => (post.style.display = "None"));
 }
+
+async function getBlacklistTags() {
+  const blacklistTagsURL = chrome.runtime.getURL("blacklist-tags.json");
+
+  return await fetch(blacklistTagsURL).then((res) => res.json());
+}
+
+function isInBlackList(post, blacklist) {
+  // It is observed that alt and title attribute contain tags
+  let tags = post.querySelector("img").alt.trim().split(" ");
+
+  return tags.some((tag) => blacklist.has(tag));
+}
+
+main();
